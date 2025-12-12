08401a322d4f04e5bccc2b24893d5dad7516ecc1
Nwachukwu Jesse Chijioke <jesfusionprox@gmail.com>
2025-12-12 06:46:48 +0100

    Feat: Add code on State-based vs. Object-oriented approaches learnt to container

--- Matplotlib_and_Seaborn/script_2.py
+++ Matplotlib_and_Seaborn/script_2.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-
 
 array_1 = np.array([2, 1, 3, 0, 233])
 
@@ -52,6 +53,144 @@ print(f"Figure Object {type(figure)}")
 print(f"Axes Object {type(axes)}")
 
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+We will demonstrate the Object-Oriented approach (Segment 1.2)
+while enforcing the Hierarchy concepts (Segment 1.1).
+"""
+
+print("\n===================================== Visualizing Data (The Architect Way) =====================================\n")
+
+# let's create fake data to work with
+t_x = np.linspace(0, 10, 100)
+
+t_y1 = np.sin(t_x)
+
+t_y2 = np.cos(t_x)
+
+"""
+Matplotlib Hierachy:
+1. The figure: This is the wall, or picture stand we're working on. It's job is to hold every drawing you create
+2. The Axes: This is the canvas we place on the figure. You can have 1 or multiple canvases
+"""
+
+figure, (axes1, axes2) = plt.subplots(nrows = 2, ncols = 1, figsize = (8, 6))
+
+print(f'''
+Type of "figure": {type(figure)}
+
+Type of "axes1": {type(axes1)}
+''')
+
+# ===================================== Plotting on canvas 1 (axes1) =====================================
+
+# we use the axes to plot, not plt
+# not plt.plot(), but axes1.plot()
+
+axes1.plot(t_x, t_y1, c = "b", label = "Sine Wave")
+
+axes1.set_title("The Sine Wave (Canvas 1)") # Note: it's .set_title(), not .title()
+
+axes1.set_ylabel("Amplitude (h)")
+axes1.legend(loc = "upper left")
+axes1.grid(True, alpha = 0.3)
+
+# ===================================== Plotting on Canvas 2 (axes2) =====================================
+
+axes2.plot(t_x, t_y2, c = "r", linestyle = "--", label = "Cosine Wave")
+
+# setting titles and labels...
+axes2.set_title("The Cosine Wave (Canvas 2)")
+axes2.set_xlabel("Time (s)")
+axes2.set_ylabel("Amplitude")
+
+
+axes2.legend(loc = "upper right")
+
+axes2.grid(True, alpha = 0.4)
+
+# we use .tight_layout() to adjust the layout so the Wall doesn't look crowded
+
+plt.tight_layout()
+
+print("Plot generated successfully using OO approach.")
+plt.show()
+
+
+
 
 
 
