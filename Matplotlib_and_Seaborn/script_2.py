bff2f8bb0e9505d7d3401b4c618ccddb3e4d8385
Nwachukwu Jesse Chijioke <jesfusionprox@gmail.com>
2025-12-18 08:11:54 +0100

    Feat: Add code on how Seaborn reads Pandas DataFrames and how Axes-level/Figure-level functions work to container
    A note was also documented on the difference between "Axes-level" functions and "Figure-level" functions

--- Matplotlib_and_Seaborn/script_2.py
+++ Matplotlib_and_Seaborn/script_2.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 array_1 = np.array([2, 1, 3, 0, 233])
 
@@ -188,6 +190,298 @@ plt.tight_layout()
 
 print("Plot generated successfully using OO approach.")
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+np.random.seed(199)
+
+# generating sample data with noise using np.random
+x_axis = np.linspace(0, 10, 100)
+
+y_axis = np.sin(x_axis) + np.random.normal(0, 0.1, 100) # we're creating a sine wave and adding a bit of noise
+
+
+
+# ===================================== LOW RESOLUTION =====================================
+
+# extracting figure and axes
+low_res_figure, low_res_axes = plt.subplots(
+    figsize = (8, 4),
+
+    dpi = 80
+)
+
+
+# Plotting on low resolution axes
+low_res_axes.plot(x_axis, y_axis, c = "orange", linestyle = '--')
+
+# setting title and labels for x & y axis...
+low_res_axes.set_title("Web Preview (Low DPI)", fontsize = 13)
+
+low_res_axes.set_xlabel("Time (s)")
+
+low_res_axes.set_ylabel("Amplitude")
+
+
+
+# ===================================== HIGH RESOLUTION =====================================
+
+high_res_figure, high_res_axes = plt.subplots(figsize = (10, 7), dpi = 150)
+
+high_res_axes.plot(x_axis, y_axis, c = 'crimson', linewidth = 2, linestyle = '--')
+
+
+high_res_axes.set_title("Print Quality (High DPI)")
+
+high_res_axes.set_xlabel("Time (s)")
+
+high_res_axes.set_ylabel("Amplitude")
+
+# ===================================== SAVING LOW RES IMAGE =====================================
+
+# saving low_res_figure as low resolution png Raster image...
+low_res_figure.savefig("sine_wave_low_pic.png", bbox_inches = "tight")
+
+
+# ===================================== SAVING HIGH RES IMAGE =====================================
+
+# saving high_res_figure as high resolution svg Vector image...
+high_res_figure.savefig("sine_wave_high_pic.svg", format = "svg", bbox_inches = "tight")
+
+
+# saving high_res_figure as high resolution pdf Vector...
+high_res_figure.savefig("sine_wave_high_pic.pdf", format = "pdf", bbox_inches = "tight")
+
+plt.show()
+
+# cleaning up memory (very Important in large scale production)
+plt.close(low_res_figure)
+plt.close(high_res_figure)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+entries = 300
+
+server_logs = pd.DataFrame({
+    "latency_ms": np.random.normal(loc = 150, scale = 30, size = entries), # Normal dist
+    "throughput_mb": np.random.uniform(low = 10, high = 100, size = entries), # Uniform dist
+    "server_region": np.random.choice(['US-East', 'EU-West'], size = entries), # Categorical
+    "status": np.random.choice(['Healthy', 'Warning'], size = entries, p = [0.8, 0.2]) # Categorical
+})
+
+
+
+# ===================================== AXES-LEVEL APPROACH =====================================
+
+# creating canvas
+
+figure, (axes_1, axes_2, axes_3) = plt.subplots(1, 3, figsize = (16, 5))
+
+# Plotting a Scatterplot on axes_1
+sns.scatterplot(data = server_logs, x = "throughput_mb", y = "latency_ms", hue = 'status', ax = axes_1)
+
+# setting the title for axes_1...
+axes_1.set_title("Network Latency vs. Throughput")
+
+
+# setting the content for axes_2
+sns.boxplot(
+    data = server_logs,
+    x = 'server_region',
+    y = 'latency_ms',
+    ax = axes_2
+)
+
+# setting the title for axes_2 and axes_3...
+axes_2.set_title("Latency Distribution by Region")
+axes_3.set_title("Testing on axes_3 with Scatterplot of sample")
+
+
+
+# setting the content for axes_3
+axes_3.scatter(
+    server_logs["throughput_mb"].sample(25),
+    server_logs["latency_ms"].sample(25),    
+    c = 'orange'
+)
+
+
+figure.suptitle("Axes-Level Functions",
+    fontsize = 19,
+    color = 'red',
+    fontweight = 'bold'
+)
+
+figure.tight_layout()
+
+plt.close(figure)
+
+
+
+
+# ===================================== FIGURE-LEVEL APPROACH =====================================
+
+fig_level = sns.relplot(
+    data = server_logs,
+    x = 'throughput_mb',
+    y = 'latency_ms',
+    hue = 'status',
+    col = 'server_region',
+    kind = 'scatter',
+    height = 4,
+    aspect = 5
+)
+
+# plt.tight_layout()
+
+fig_level.fig.suptitle('Latency Analysis Faceted by Region', y = 1.03)
+
+plt.show()
+
 
 
 
