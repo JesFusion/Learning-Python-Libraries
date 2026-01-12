8b6af5bb0519b76dedb471420a9f156cd4427255
Nwachukwu Jesse Chijioke <jesfusionprox@gmail.com>
2026-01-12 10:23:51 +0100

    Feat: Add new script_1.sh files for Linux and Docker
    A docker_init.sh file was also created for creating folders with files to prctice Docker in

---
+++ Docker/docker_init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+cd /home/jesfusion/Documents/ml/ML-Learning-Repository/Docker/Docker_Folders/
+
+echo "Enter folder name: "
+
+read folder_name
+
+mkdir $folder_name
+
+cd $folder_name
+
+touch Dockerfile
+touch .dockerignore
+touch build.sh
+
+echo "Done creating folder '$folder_name' with files"
+
+
+
+
+
+
